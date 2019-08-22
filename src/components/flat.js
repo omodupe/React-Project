a7270f08cfa594e5d19318908de95ef1658a167b
omodupe <olowolafemodupe@gmail.com>
2019-08-22 15:38:36 +0100

    first commit added flat.js component

---
+++ src/components/flat.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Flat extends React.Component{
+    render(){
+        return(
+            <div className='flat'>
+                <div className='flat-picture'>Hi flat mate</div>
+                <div className='flat-title'>Flat</div>
+            </div>
+        )
+    }
+}
+
+export default Flat;
