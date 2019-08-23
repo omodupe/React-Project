05fc9178352d556d0cffaae454b331458f902130
omodupe <olowolafemodupe@gmail.com>
2019-08-23 18:10:20 +0100

    Add styling to project

--- src/App.js
+++ src/App.js
@@ -4,12 +4,24 @@ import Flat from './components/flat';
 
 class App extends Component {
   render() {
+    const flat= {
+      "name": "Superbe vue à 2 min du Sacré Coeur",
+      "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat5.jpg",
+      "price": 135,
+      "priceCurrency": "EUR"
+    };
 
     return (
       <div className="App">
-        <div>
-          <Flat />
+        <div className="main">
+          <div className="search"></div>
+          <div className="flats">
+          <Flat flat={flat} />
+          <Flat flat={flat} />
+          <Flat flat={flat} />
+          </div>
         </div>
+        <div className="map"></div>
       </div>
     )
   }
--- src/components/flat.js
+++ src/components/flat.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import './flat.css';
 
 class Flat extends React.Component{
     render(){
+    
+        const title= `${this.props.flat.price}${this.props.flat.priceCurrency} ${this.props.flat.name}`;
+        const style= {
+            backgroundImage: `url('${this.props.flat.imageUrl}')`
+        }
+
         return(
             <div className='flat'>
-                <div className='flat-picture'>Hi flat mate</div>
-                <div className='flat-title'>Flat</div>
+                <div className='flat-picture' style={style}></div>
+                <div className='flat-title'>{title}</div>
             </div>
         )
     }
